86e7009683b5a30fa013ef5e48ae9f59a7f4c149
이병구 <lee@ibyeong-guui-Macmini.local>
2023-09-08 16:56:31 +0900

    connect js to html and created screen.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./screen.js";
 
+const canvas = document.querySelector("#game_canvas");
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+const context = canvas.getContext("2d");
+
+let frameCount = 0;
