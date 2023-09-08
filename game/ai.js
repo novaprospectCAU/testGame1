218f3bc51232e63cae187db6a1c3d433b7e4edb8
이병구 <lee@ibyeong-guui-Macmini.local>
2023-09-08 15:35:01 +0900

    added primitive ai algorithm

--- game/ai.js
+++ game/ai.js
@@ -2,3 +2,31 @@ let ghostX = 4;
 let ghostY = 6;
 
 let before = [ghostX, ghostY];
+
+const ai_move = () => {
+    while(true) {
+        let nextX = 0;
+        let nextY = 0;
+        if (Math.random() > 0.5) {
+            nextX = Math.floor(Math.random() * 2 - 1);
+            if (nextX === 0) {
+                nextY = Math.floor(Math.random() * 2 - 1);
+                if (nextY === 0)
+                    continue;
+            }
+        }
+        else {
+            nextY = Math.floor(Math.random() * 2 - 1);
+            if (nextY === 0) {
+                nextX = Math.floor(Math.random() * 2 - 1);
+                if (nextX === 0)
+                    continue;
+            }
+        }
+        before[0] = ghostX;
+        before[1] = ghostY;
+        ghostX += nextX;
+        ghostY += nextY;
+    }
+
+}
