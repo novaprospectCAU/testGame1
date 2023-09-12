b1395522ee8c604f973535b21a24d827a22f6295
novaprospectCAU <novaprospect110a@gmail.com>
2023-09-12 13:54:18 +0900

    added enemy_movement ai export

--- game/enemy.js
+++ game/enemy.js
@@ -1,13 +1,15 @@
+import { ai_move } from "./ai.js";
+
 export class Enemy {
-    HIT_WIDTH = 20;
-    HIT_HEIGHT = 20;
-    constructor(posX = 4, posY = 6) {
-        this.X = posX;
-        this.Y = posY;
-        this.status = "ghost";
-    }
-    updatePosition(x, y) {
-        this.X = x;
-        this.Y = y;
-    }
+  HIT_WIDTH = 20;
+  HIT_HEIGHT = 20;
+  constructor(posX = 4, posY = 6) {
+    this.X = posX;
+    this.Y = posY;
+    this.status = "ghost";
+  }
+  updatePosition(x, y) {
+    this.X = x;
+    this.Y = y;
+  }
 }
